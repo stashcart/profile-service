7755ecf29e080c0d4f9e51a6e68f20d0a59e433e
vladhuk <vladhookovskiy@gmail.com>
2021-09-03 02:00:22 +0300

    - improve dto validators;

--- src/profiles/dto/profile-patch.request.dto.ts
+++ src/profiles/dto/profile-patch.request.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDefined, IsOptional, IsString, Matches } from 'class-validator';
+import { IsOptional, IsString, Matches } from 'class-validator';
 
 export class ProfilePatchRequestDto {
   @ApiProperty({ example: 'John Wick' })
