c569193a2f0223f70fbb476b969405455e703a3a
vladhuk <vladhookovskiy@gmail.com>
2021-12-01 16:33:21 +0200

    - update dto;

--- src/profiles/dto/create-profile.request.dto.ts
+++ src/profiles/dto/create-profile.request.dto.ts
@@ -1,14 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, Matches } from 'class-validator';
+import { IsEmail, IsOptional, IsString, Matches } from 'class-validator';
 
 export class CreateProfileRequestDto {
+  @ApiProperty({
+    example: `user-${Math.floor(Math.random() * 10000)}@gmail.com`,
+  })
   @IsEmail()
   email!: string;
 
-  name!: string;
+  @ApiProperty({ example: 'John Wick' })
+  @IsOptional()
+  @IsString()
+  name?: string;
 
   @ApiProperty({ example: '0981234567' })
+  @IsOptional()
   @Matches(/\d{10}/)
   @IsString()
-  phone!: string;
+  phone?: string;
 }
